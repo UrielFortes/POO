70193e950f9fa7f9c7cd8627d5672e780daff5d0
Uriel Fortes <82785719+UrielFortes@users.noreply.github.com>
2024-12-12 13:04:01 -0300

    Change Bomb
    change velocity of bomb

--- Assets/Scripts/Scripts Scene 2/Bomb.cs
+++ Assets/Scripts/Scripts Scene 2/Bomb.cs
@@ -4,23 +4,29 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    
+    public Rigidbody rb;
+    public float strengthVelocity;
+
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     
     void Update()
     {
-        Destroy(this.gameObject, 1.85f);
+        Destroy(this.gameObject, 0.85f);
+
+        rb.velocity =  transform.up * strengthVelocity;
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("Colidiu com o Player");
+            Debug.Log("Colidiu no Player");
         };
     }
+
 }
